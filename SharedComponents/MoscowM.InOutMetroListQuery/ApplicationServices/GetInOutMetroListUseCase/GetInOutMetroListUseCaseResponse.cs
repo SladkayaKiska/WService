474a0335e41aa302acf34baa7a1f162085be7a9d
SladkayaKiska <49351091+SladkayaKiska@users.noreply.github.com>
2020-05-24 17:45:07 +0300

    Add files via upload

---
+++ SharedComponents/MoscowM.InOutMetroListQuery/ApplicationServices/GetInOutMetroListUseCase/GetInOutMetroListUseCaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using MoscowM.DomainObjects;
+using MoscowM.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoscowM.ApplicationServices.GetInOutMetroListUseCase
+{
+    public class GetInOutMetroListUseCaseResponse : UseCaseResponse
+    {
+        public IEnumerable<InOutMetro> InOutMetros { get; }
+
+        public GetInOutMetroListUseCaseResponse(IEnumerable<InOutMetro> inoutmetros) => InOutMetros = inoutmetros;
+    }
+}
