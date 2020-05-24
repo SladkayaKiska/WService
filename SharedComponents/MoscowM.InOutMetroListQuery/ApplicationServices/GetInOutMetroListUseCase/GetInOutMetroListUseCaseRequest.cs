1f7f6a8171ba26a0e6a37fd6286665570462ee16
SladkayaKiska <49351091+SladkayaKiska@users.noreply.github.com>
2020-05-24 17:43:04 +0300

    Add files via upload

---
+++ SharedComponents/MoscowM.InOutMetroListQuery/ApplicationServices/GetInOutMetroListUseCase/GetInOutMetroListUseCaseRequest.cs
@@ -0,0 +1,30 @@
+﻿using MoscowM.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoscowM.ApplicationServices.GetInOutMetroListUseCase
+{
+    public class GetInOutMetroListUseCaseRequest : IUseCaseRequest<GetInOutMetroListUseCaseResponse>
+    {
+        public string MetroStation { get; private set; }
+        public long? InOutMetroId { get; private set; }
+
+        private GetInOutMetroListUseCaseRequest()
+        { }
+
+        public static GetInOutMetroListUseCaseRequest CreateAllInOutMetrosRequest()
+        {
+            return new GetInOutMetroListUseCaseRequest();
+        }
+
+        public static GetInOutMetroListUseCaseRequest CreateInOutMetroRequest(long inoutmetroId)
+        {
+            return new GetInOutMetroListUseCaseRequest() { InOutMetroId = inoutmetroId };
+        }
+        public static GetInOutMetroListUseCaseRequest CreateOrganizationInOutMetrosRequest(string metrostation)
+        {
+            return new GetInOutMetroListUseCaseRequest() { MetroStation = metrostation };
+        }
+    }
+}
---
+++ SharedComponents/MoscowM.InOutMetroListQuery/ApplicationServices/GetInOutMetroListUseCase/MetroStationCriteria.cs
@@ -0,0 +1,20 @@
+﻿using MoscowM.DomainObjects;
+using MoscowM.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MoscowM.ApplicationServices.GetInOutMetroListUseCase
+{
+    public class MetroStationCriteria : ICriteria<InOutMetro>
+    {
+        public string MetroStation { get; }
+
+        public MetroStationCriteria(string metrostation)
+            => MetroStation = metrostation;
+
+        public Expression<Func<InOutMetro, bool>> Filter
+            => (iom => iom.MetroStation == MetroStation);
+    }
+}
